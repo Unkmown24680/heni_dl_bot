985aa04f512e572ab75f7985fde8217185cae540
Unkmown24680 <144600694+Unkmown24680@users.noreply.github.com>
2024-04-07 15:57:38 +0530

    Update start.py

--- plugin/start.py
+++ plugin/start.py
@@ -2,4 +2,4 @@ from pyrogram import *
 from pyrogram.types import *
 
 def start(client, message):
-    message.reply_text("""**/search <hentai name> to search""", parse_mode="markdown")
+    message.reply_text("""**/search <hentai name> to search""", parse_mode="Markdown")
